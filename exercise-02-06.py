4060e32df56beb8488904fe75750fd89fc05e4b9
Amir Maroufian <amirmaroufian@gmail.com>
2026-01-29 00:36:45 +0330

    add exercise-02-06: two random combinations

---
+++ exercise-02-06.py
@@ -0,0 +1,7 @@
+import random
+
+code1 = str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
+code2 = str(random.randint(1,6)) + str(random.randint(1,6)) + str(random.randint(1,6)) + str(random.randint(1,6))
+
+print("3-digit code:", code1)
+print("4-digit code:", code2)
