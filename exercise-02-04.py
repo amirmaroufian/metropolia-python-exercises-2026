6188b2a8caa3c16ec99df17913071a50711acf23
Amir Maroufian <amirmaroufian@gmail.com>
2026-01-28 23:06:55 +0330

    add exercise-02-04: numbers sum, product and average

---
+++ exercise-02-04.py
@@ -0,0 +1,9 @@
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+
+sum_of_all = number1 + number2 + number3
+product = number1 * number2 * number3
+average = sum_of_all / 3
+
+print(f"Sum of all numbers is: {sum_of_all}, product is: {product}, average is: {average}")
